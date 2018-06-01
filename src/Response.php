e083918a91cb8950fb70f3462300bed2eb8b28d4
sorinsarca <sarca_sorin@hotmail.com>
2018-06-01 16:16:12 +0300

    Updated

--- src/Response.php
+++ src/Response.php
@@ -17,24 +17,115 @@
 
 namespace Opis\Http;
 
-use Psr\Http\Message\ResponseInterface;
+use InvalidArgumentException;
+use Psr\Http\Message\{
+    ResponseInterface, StreamInterface
+};
 
 class Response extends Message implements ResponseInterface
 {
+    /** @var int */
+    protected $statusCode;
+
+    /** @var string */
+    protected $reasonPhrase = '';
+
+    const HTTP_STATUS = [
+        // 1xx
+        100 => 'Continue',
+        101 => 'Switching Protocols',
+        102 => 'Processing',
+        103 => 'Early Hints',
+        // 2xx
+        200 => 'OK',
+        201 => 'Created',
+        202 => 'Accepted',
+        203 => 'Non-Authoritative Information',
+        204 => 'No Content',
+        205 => 'Reset Content',
+        206 => 'Partial Content',
+        207 => 'Multi-Status',
+        208 => 'Already Reported',
+        226 => 'IM Used',
+        // 3xx
+        300 => 'Multiple Choices',
+        301 => 'Moved Permanently',
+        302 => 'Found',
+        303 => 'See Other',
+        304 => 'Not Modified',
+        305 => 'Use Proxy',
+        306 => '(Unused)',
+        307 => 'Temporary Redirect',
+        308 => 'Permanent Redirect',
+        // 4xx
+        400 => 'Bad Request',
+        401 => 'Unauthorized',
+        402 => 'Payment Required',
+        403 => 'Forbidden',
+        404 => 'Not Found',
+        405 => 'Method Not Allowed',
+        406 => 'Not Acceptable',
+        407 => 'Proxy Authentication Required',
+        408 => 'Request Timeout',
+        409 => 'Conflict',
+        410 => 'Gone',
+        411 => 'Length Required',
+        412 => 'Precondition Failed',
+        413 => 'Payload Too Large',
+        414 => 'URI Too Long',
+        415 => 'Unsupported Media Type',
+        416 => 'Range Not Satisfiable',
+        417 => 'Expectation Failed',
+        418 => 'I\'m a teapot',
+        421 => 'Misdirected Request',
+        422 => 'Unprocessable Entity',
+        423 => 'Locked',
+        424 => 'Failed Dependency',
+        425 => 'Unordered Collection',
+        426 => 'Upgrade Required',
+        428 => 'Precondition Required',
+        429 => 'Too Many Requests',
+        431 => 'Request Header Fields Too Large',
+        444 => 'Connection Closed Without Response',
+        451 => 'Unavailable For Legal Reasons',
+        // 5xx
+        500 => 'Internal Server Error',
+        501 => 'Not Implemented',
+        502 => 'Bad Gateway',
+        503 => 'Service Unavailable',
+        504 => 'Gateway Timeout',
+        505 => 'HTTP Version Not Supported',
+        506 => 'Variant Also Negotiates',
+        507 => 'Insufficient Storage',
+        508 => 'Loop Detected',
+        510 => 'Not Extended',
+        511 => 'Network Authentication Required',
+    ];
+
     /**
-     * @inheritDoc
+     * Response constructor.
+     * @param null|string|resource|StreamInterface $body
+     * @param int $status
+     * @param array|null $headers
+     * @param string|null $protocolVersion
      */
-    public function getStatusCode()
-    {
-        // TODO: Implement getStatusCode() method.
+    public function __construct(
+        $body = "php://memory",
+        int $status = 200,
+        array $headers = null,
+        string $protocolVersion = null
+    ) {
+        parent::__construct($body, $headers, $protocolVersion);
+        $this->statusCode = $status;
+        $this->reasonPhrase = self::HTTP_STATUS[$status] ?? '';
     }
 
     /**
      * @inheritDoc
      */
-    public function withStatus($code, $reasonPhrase = '')
+    public function getStatusCode()
     {
-        // TODO: Implement withStatus() method.
+        return $this->statusCode;
     }
 
     /**
@@ -42,7 +133,29 @@ class Response extends Message implements ResponseInterface
      */
     public function getReasonPhrase()
     {
-        // TODO: Implement getReasonPhrase() method.
+        return $this->reasonPhrase;
     }
 
+    /**
+     * @inheritDoc
+     */
+    public function withStatus($code, $reasonPhrase = '')
+    {
+        if (!is_int($code)) {
+            throw new InvalidArgumentException("Status code must be an integer");
+        }
+        if (!is_string($reasonPhrase)) {
+            throw new InvalidArgumentException("Reason phrase must be a string");
+        }
+
+        if ($reasonPhrase === '') {
+            $reasonPhrase = self::HTTP_STATUS[$code] ?? '';
+        }
+
+        $response = clone $this;
+        $response->statusCode = $code;
+        $response->reasonPhrase = $reasonPhrase;
+
+        return $response;
+    }
 }
