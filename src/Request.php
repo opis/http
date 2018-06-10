3d82596973a88f8dd96c9e6e9289b2bde40f42ba
Marius Sarca <marius.sarca@gmail.com>
2018-06-10 16:48:32 +0300

    Moved method & removed class

--- src/Request.php
+++ src/Request.php
@@ -260,4 +260,49 @@ class Request
 
         return $this->formData;
     }
+
+    /**
+     * @return Request
+     */
+    public static function fromGlobals(): self
+    {
+        $method = strtoupper($_SERVER['REQUEST_METHOD'] ?? 'GET');
+
+        if ($method === 'POST') {
+            if (isset($_SERVER['HTTP_X_HTTP_METHOD_OVERRIDE'])) {
+                $method = strtoupper($_SERVER['HTTP_X_HTTP_METHOD_OVERRIDE']);
+            } elseif (isset($_POST['x_http_method_override'])) {
+                $method = strtoupper($_POST['x_http_method_override']);
+            }
+        }
+
+        $headers = [];
+
+        foreach ($_SERVER as $key => $value) {
+            if (!is_scalar($value)) {
+                continue;
+            }
+            if (strpos($key, 'HTTP_') === 0) {
+                $key = substr($key, 5);
+            } elseif (!in_array($key, ['CONTENT_LENGTH', 'CONTENT_MD5', 'CONTENT_TYPE'])) {
+                continue;
+            }
+            $key = implode('-', explode('_', $key));
+            $headers[$key] = $value;
+        }
+
+        if (isset($_SERVER['PATH_INFO'])) {
+            $requestTarget = $_SERVER['PATH_INFO'];
+            if (isset($_SERVER['QUERY_STRING'])) {
+                $requestTarget .= '?' . $_SERVER['QUERY_STRING'];
+            }
+        } else {
+            $requestTarget = $_SERVER['REQUEST_URI'] ?? '/';
+        }
+
+        $protocol = $_SERVER['SERVER_PROTOCOL'] ?? 'HTTP/1.1';
+        $secure = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off';
+
+        return new self($method, $requestTarget, $protocol, $secure, $headers, $_FILES, null, $_COOKIE, $_GET, $_POST);
+    }
 }
