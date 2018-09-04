32b2ba5cf4f6baee52be134f7f28d4ef588b0ddd
Marius Sarca <marius.sarca@gmail.com>
2018-09-04 22:32:53 +0300

    Fixed copyright

--- autoload.php
+++ autoload.php
@@ -1,9 +1,6 @@
 <?php
 /* ===========================================================================
- * Opis Project
- * http://opis.io
- * ===========================================================================
- * Copyright 2013-2015 Marius Sarca
+ * Copyright 2018 Zindex Software
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
