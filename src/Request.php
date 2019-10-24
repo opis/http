2c84576319f4c3356252042f2087c7806ea848c0
Marius Sarca <marius.sarca@gmail.com>
2019-10-24 22:45:18 +0300

    Fixed missing authority

--- src/Request.php
+++ src/Request.php
@@ -136,6 +136,7 @@ class Request extends Message
                     }
                     $components['host'] = $host;
                     $components['port'] = $port;
+                    $components['authority'] = $port === null ? $host : $host . ':' . $port;
                 }
             }
 
